999157d7c3f82803188797c5aa690048ab7180a0
s1131225 <lucyamy23780983@gmail.com>
2025-11-24 15:43:49 +0800

    V.2 新增資料(自動產生文件)

--- app/src/main/java/tw/edu/pu/csim/tcyang/firestore1124/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/tcyang/firestore1124/MainActivity.kt
@@ -19,12 +19,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             FireStore1124Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                UserScoreScreen()
             }
         }
     }
