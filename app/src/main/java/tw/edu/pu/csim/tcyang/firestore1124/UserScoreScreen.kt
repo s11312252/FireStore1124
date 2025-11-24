d34ad3f5f18134b31ba5793d4ce2946b18458647
s1131225 <lucyamy23780983@gmail.com>
2025-11-24 16:13:21 +0800

    V.3 新增/異動/刪除資料(自動產生文件)

--- app/src/main/java/tw/edu/pu/csim/tcyang/firestore1124/UserScoreScreen.kt
+++ app/src/main/java/tw/edu/pu/csim/tcyang/firestore1124/UserScoreScreen.kt
@@ -19,12 +19,29 @@ fun UserScoreScreen( userScoreViewModel: UserScoreViewModel = viewModel()
     ) {
         Button(onClick = {
             // 在按鈕點擊時，直接呼叫 ViewModel 的函式
-            var userScore = UserScoreModel("陳芯霈", 100)
+            var userScore = UserScoreModel("陳芯霈", 39)
             userScoreViewModel.addUser(userScore)
         }) {
             Text("新增資料")
         }
+        Button(onClick = {
+            // 在按鈕點擊時，直接呼叫 ViewModel 的函式
+            var userScore = UserScoreModel("陳芯霈", 21)
+            userScoreViewModel.updateUser(userScore)
+        }) {
+            Text("新增/異動資料")
+        }
+
+        Button(onClick = {
+            // 在按鈕點擊時，直接呼叫 ViewModel 的函式
+            var userScore = UserScoreModel("陳芯霈", 21)
+            userScoreViewModel.deleteUser(userScore)
+        }) {
+            Text("刪除資料")
+        }
         Text(userScoreViewModel.message)
+
+        }
     }
-}
+
 
